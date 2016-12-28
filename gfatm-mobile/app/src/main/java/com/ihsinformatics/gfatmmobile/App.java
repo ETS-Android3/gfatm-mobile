e93d7eb7cfe58455dcc68b3101b27f97e75364d4
Rabbia Hassan <rabbia.hassan@ihsinformatics.com>
2016-12-28 16:04:15 +0500

    15/12/2016 Reformat Code

--- gfatm-mobile/app/src/main/java/com/ihsinformatics/gfatmmobile/App.java
+++ gfatm-mobile/app/src/main/java/com/ihsinformatics/gfatmmobile/App.java
@@ -36,6 +36,10 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import static java.util.Calendar.DATE;
+import static java.util.Calendar.MONTH;
+import static java.util.Calendar.YEAR;
+
 
 /**
  * @author owais.hussain@irdresearch.org
@@ -327,6 +331,23 @@ public class App {
      */
     public static String getSqlDateTime(Date date) {
         return DateFormat.format("yyy-MM-dd hh:mm:ss", date).toString();
+    }
+
+    public static int getDiffYears(Date first, Date last) {
+        Calendar a = getCalendar(first);
+        Calendar b = getCalendar(last);
+        int diff = b.get(YEAR) - a.get(YEAR);
+        if (a.get(MONTH) > b.get(MONTH) ||
+                (a.get(MONTH) == b.get(MONTH) && a.get(DATE) > b.get(DATE))) {
+            diff--;
+        }
+        return diff;
+    }
+
+    public static Calendar getCalendar(Date date) {
+        Calendar cal = Calendar.getInstance(Locale.US);
+        cal.setTime(date);
+        return cal;
     }
 
 }
