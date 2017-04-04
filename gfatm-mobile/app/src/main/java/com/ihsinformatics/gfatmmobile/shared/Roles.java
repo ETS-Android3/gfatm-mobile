08680c3ca594affd97c6f6466e9efb0da0e2d8f0
Rabbia Hassan <rabbia.hassan@ihsinformatics.com>
2017-04-04 10:39:17 +0500

    Roles Added

---
+++ gfatm-mobile/app/src/main/java/com/ihsinformatics/gfatmmobile/shared/Roles.java
@@ -0,0 +1,50 @@
+/* Copyright(C) 2015 Interactive Health Solutions, Pvt. Ltd.
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 3 of the License (GPLv3), or any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program; if not, write to the Interactive Health Solutions, info@ihsinformatics.com
+You can also access the license on the internet at the address: http://www.gnu.org/licenses/gpl-3.0.html
+
+Interactive Health Solutions, hereby disclaims all copyright interest in this program written by the contributors. */
+
+package com.ihsinformatics.gfatmmobile.shared;
+
+public class Roles {
+
+    public static final String DEVELOPER = "System Developer";
+
+    public static final String PET_PROGRAM_MANAGER = "PET Program Manager";
+    public static final String PET_FIELD_SUPERVISOR = "PET Field Supervisor";
+    public static final String PET_HEALTH_WORKER = "PET Health Worker";
+    public static final String PET_CLINICIAN = "PET Clinician";
+    public static final String PET_PSYCHOLOGIST = "PET Psychologist";
+
+    public static final String FAST_PROGRAM_MANAGER = "FAST Program Manager";
+    public static final String FAST_FIELD_SUPERVISOR = "FAST Field Supervisor";
+    public static final String FAST_SCREENER = "FAST Screener";
+    public static final String FAST_FACILITATOR = "FAST Facilitator";
+    public static final String FAST_LAB_TECHNICIAN = "FAST Lab Technician";
+    public static final String FAST_SITE_MANAGER = "FAST Site Manager";
+
+    public static final String COMORBIDITIES_PROGRAM_MANAGER = "Comorbidities Program Manager";
+    public static final String COMORBIDITIES_DIABETES_EDUCATOR = "Comorbidities Diabetes Educator";
+    public static final String COMORBIDITIES_COUNSELOR = "Comorbidities Counselor";
+    public static final String COMORBIDITIES_HEALTH_WORKER = "Comorbidities Health Worker";
+
+    public static final String PMDT_PROGRAM_MANAGER = "PMDT Program Manager";
+    public static final String PMDT_TREATMENT_SUPPORT = "PMDT Treatment Supporter";
+    public static final String PMDT_TREATMENT_COORDINATOR = "PMDT Treatment Coordinator";
+    public static final String PMDT_REFERRAL_SITE_COORDINATOR = "PMDT Referral Site Coordinator";
+    public static final String PMDT_LAB_TECHNICIAN = "PMDT Lab Technician";
+    public static final String PMDT_DOT_PROVIDER = "PMDT DOT Provider";
+
+    public static final String CHILDHOODTB_PROGRAM_MANAGER = "ChildhoodTB Program Manager";
+    public static final String CHILDHOODTB_MEDICAL_OFFICER = "ChildhoodTB Medical Officer";
+    public static final String CHILDHOODTB_MONITOR = "ChildhoodTB Monitor";
+    public static final String CHILDHOODTB_NURSE = "ChildhoodTB Nurse";
+    public static final String CHILDHOODTB_PROGRAM_ASSISTANT = "ChildhoodTB Program Assistant";
+
+
+}
