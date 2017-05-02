45c2301fccb7a2a0dd463229c0485638f2d58290
Rabbia Hassan <rabbia.hassan@ihsinformatics.com>
2017-05-02 13:42:55 +0500

    02/05/2017

--- gfatm-mobile/app/src/main/java/com/ihsinformatics/gfatmmobile/fast/FastAfbSmearMicroscopyOrderAndResultForm.java
+++ gfatm-mobile/app/src/main/java/com/ihsinformatics/gfatmmobile/fast/FastAfbSmearMicroscopyOrderAndResultForm.java
@@ -37,6 +37,7 @@ import android.widget.TextView;
 
 import com.ihsinformatics.gfatmmobile.AbstractFormActivity;
 import com.ihsinformatics.gfatmmobile.App;
+import com.ihsinformatics.gfatmmobile.MainActivity;
 import com.ihsinformatics.gfatmmobile.R;
 import com.ihsinformatics.gfatmmobile.custom.MySpinner;
 import com.ihsinformatics.gfatmmobile.custom.MyTextView;
@@ -715,7 +716,13 @@ public class FastAfbSmearMicroscopyOrderAndResultForm extends AbstractFormActivi
                 loading.dismiss();
 
                 if (result.equals("SUCCESS")) {
-                    resetViews();
+                    MainActivity.backToMainMenu();
+                    try {
+                        InputMethodManager imm = (InputMethodManager) context.getSystemService(context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(mainContent.getWindowToken(), 0);
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
 
                     final AlertDialog alertDialog = new AlertDialog.Builder(context, R.style.dialog).create();
                     alertDialog.setMessage(getResources().getString(R.string.form_submitted));
--- gfatm-mobile/app/src/main/java/com/ihsinformatics/gfatmmobile/fast/FastTreatmentInitiationForm.java
+++ gfatm-mobile/app/src/main/java/com/ihsinformatics/gfatmmobile/fast/FastTreatmentInitiationForm.java
@@ -31,6 +31,7 @@ import android.widget.TextView;
 
 import com.ihsinformatics.gfatmmobile.AbstractFormActivity;
 import com.ihsinformatics.gfatmmobile.App;
+import com.ihsinformatics.gfatmmobile.MainActivity;
 import com.ihsinformatics.gfatmmobile.R;
 import com.ihsinformatics.gfatmmobile.custom.MySpinner;
 import com.ihsinformatics.gfatmmobile.custom.TitledButton;
@@ -588,7 +589,13 @@ public class FastTreatmentInitiationForm extends AbstractFormActivity implements
                 loading.dismiss();
 
                 if (result.equals("SUCCESS")) {
-                    resetViews();
+                    MainActivity.backToMainMenu();
+                    try {
+                        InputMethodManager imm = (InputMethodManager) context.getSystemService(context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(mainContent.getWindowToken(), 0);
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
 
                     final AlertDialog alertDialog = new AlertDialog.Builder(context, R.style.dialog).create();
                     alertDialog.setMessage(getResources().getString(R.string.form_submitted));
