0f652de2d2761f76cd75b0ea0c658a1a8be061e6
Rabbia Hassan <rabbia.hassan@ihsinformatics.com>
2018-02-07 12:39:08 +0500

    Enumeration form submission request type added

--- gfatm-mobile/app/src/main/java/com/ihsinformatics/gfatmmobile/shared/RequestType.java
+++ gfatm-mobile/app/src/main/java/com/ihsinformatics/gfatmmobile/shared/RequestType.java
@@ -13,6 +13,10 @@ public class RequestType {
     /* FAST form types */
     public static final String FAST_SCREENING = "fast_screening";
 
+    /* ZTTS form types */
+    public static final String ZTTS_ENUMERATION_BLOCK = "ztts_enumeration_block_level";
+    public static final String ZTTS_ENUMERATION_HOUSEHOLD = "ztts_enumeration_household_level";
+
     /* miscellaneous form types */
     public static final String GFATM_SEARCH = "gfatm_search";
     public static final String GFATM_GET_LOCATION = "gfatm_get_location";
